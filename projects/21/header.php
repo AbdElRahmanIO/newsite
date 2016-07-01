d76c7ed04d7beaad846527a20f1fb6bbc6628c7b
AbdElRahman Ahmad <AbdElRahman Ahmad>
2016-07-01 07:09:59 +0200

    upload proj 21 zafat

---
+++ projects/21/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>زفات | زفات ليالي العمر</title>
+
+    <!--bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css">
+
+    <!--font-awesome-->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+    <!--slider-->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    
+    <!--myfiles-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/querys.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<header id="main">
+
+<div class="container-fluid topbar">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="log">
+          <a href=""><i class="fa fa-sign-out" aria-hidden="true"></i> دخول</a>
+          <a href=""><i class="fa fa-user-plus" aria-hidden="true"></i> تسجيل</a>
+        </div><!-- log -->
+      </div><!-- col-xs-12 -->
+    </div><!-- row -->
+  </div><!-- container -->
+</div><!-- container-fluid topbar -->
+
+
+
+
+
+<nav class="navbar navbar-default btnbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" class="img-responsive"></a>
+    </div><!-- navbar-header -->
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">الرئيسية <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">الزفات</a></li>
+        <li><a href="#">منتجاتنا</a></li>
+        <li><a href="#">فريقنا</a></li>
+        <li><a href="#">أسئلة شائعة</a></li>
+        <li><a href="#">انضم إلينا</a></li>
+        <li><a href="#">اتصل بنا</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">قائمة منزلقة <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">الرئيسية</a></li>
+            <li><a href="#">الزفات</a></li>
+            <li><a href="#">منتجاتنا</a></li>
+            <li><a href="#">فريقنا</a></li>
+            <li><a href="#">أسئلة شائعة</a></li>
+            <li><a href="#">انضم إلينا</a></li>
+            <li><a href="#">اتصل بنا</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav><!-- navbar -->
+
+
+</header><!-- #main -->
