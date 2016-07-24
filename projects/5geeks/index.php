c5a4edb9fc0938a7b6c360e6acda771e6fbf1c13
AbdElRahman Ahmad <AbdElRahman Ahmad>
2016-07-24 13:48:00 +0300

    5geeks and gamalheshmat

---
+++ projects/5geeks/index.php
@@ -0,0 +1,90 @@
+<?php include 'header.php'; ?>
+
+
+
+
+
+
+
+
+
+<div class="container-fluid slideshow">
+	<div class="row">
+		<div class="col-xs-12">
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner" role="listbox">
+			    <div class="item active">
+			      <img src="..." alt="...">
+			      <div class="carousel-caption">
+			        <h3>عنوان رئيسي للسلايد</h3>
+			        <p>نص فرعي تفصيلي للسلايد</p>
+			      </div><!-- carousel-caption -->
+			    </div><!-- item -->
+			  </div><!-- carousel-inner -->
+
+			  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div><!-- slide -->
+		</div><!-- col-xs-12 -->
+	</div><!-- row -->
+</div><!-- container-fluid -->
+
+
+
+
+
+
+
+
+
+
+<div class="container welcome">
+	<div class="row">
+		<div class="col-xs-12">
+			<h1><span>WELCOME</span> TO OUR COMPANY!</h1>
+			<p>We’re focused on delivering excellent levels of service with fast and reliable web servers ,along with helpful and friendly customer support from our own offices in the US and Europe. We live, sleep and breathe web hosting.</p>
+		</div><!-- col-xs-12 -->
+	</div><!-- row -->
+</div><!-- container welcome -->
+
+
+
+
+
+
+
+
+
+
+<div class="container curses">
+	<div class="row">
+
+		<div class="col-sm-6">
+			<div class="curse">
+				<i class="fa fa-bluetooth" aria-hidden="true"></i>
+				<h2>اسم الكورس هنا</h2>
+				<p>تفاصيل الكورس هنا</p>
+				<a href=""><button>إعرف المزيد</button></a>
+			</div><!-- curse -->
+		</div><!-- col-sm-6 -->
+
+		<div class="col-sm-6">
+			<div class="curse">
+				<i class="fa fa-cloud" aria-hidden="true"></i>
+				<h2>اسم الكورس هنا</h2>
+				<p>تفاصيل الكورس هنا</p>
+				<a href=""><button>إعرف المزيد</button></a>
+			</div><!-- curse -->
+		</div><!-- col-sm-6 -->
+
+	</div><!-- row -->
+</div><!-- container curses -->
+
+
+<?php include 'footer.php'; ?>
