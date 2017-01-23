5c69a2e43f4f576a120252321cd6af9f6f42c31c
Abdelrahman Ahmad <abdelrahman.ahmad@Intermark-corp.local>
2017-01-23 16:00:14 +0200

    add screens of temps + gahez

--- old/projects/21/audiolist.php
+++ old/projects/21/audiolist.php
@@ -41,6 +41,186 @@
 					</div><!-- audioitem -->
 				</div><!-- col-sm-6 -->
 
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
+				<div class="col-sm-6">
+					<div class="audioitem">
+						<div class="icontop"><i class="fa fa-headphones" aria-hidden="true"></i></div>
+						<div class="row">
+							<div class="col-sm-4">
+								<div class="audioitemimg">
+									<a href=""><img src="img/audio.png"></a>
+								</div><!-- audioitemimg -->
+							</div><!-- col-sm-4 -->
+							<div class="col-sm-8">
+								<div class="audioiteminfo">
+									<a href=""><h1>عنوان المقطع الصوتي</h1></a>
+									<p>إنشاد : <a href="">اسم المنشد</a></p>
+									<p>تلحين وتوزيع : <a href="">اسم الموزع</a></p>
+								</div><!-- audioiteminfo -->
+							</div><!-- col-sm-8 -->
+						</div><!-- row -->
+					</div><!-- audioitem -->
+				</div><!-- col-sm-6 -->
+
 			</div><!-- listaudiocontent row -->
 		</div><!-- col-xs-12 -->
 	</div><!-- row -->
