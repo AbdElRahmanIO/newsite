e95f35e1a7e6ddf4924b0ce56c7697d9ff3773b9
Abdelrahman Ahmad <abdelrahman.ahmad@Intermark-corp.local>
2017-02-01 13:57:32 +0200

    changes at experience

--- old/sections/experience.php
+++ old/sections/experience.php
@@ -4,32 +4,32 @@
 <ul id="jobs">
 	<li>
 		<div class="details">
-			<h3>interMark GSW</h3>
-			<h4>Front-End Web Developer - Cairo, Egypt</h4>
-			<h5>July 2016 - Present</h5>
+			<h3>interMark</h3>
+			<h4>UI Developer - Cairo, Egypt</h4>
+			<h5>November 2016 - Present</h5>
 		</div>
 		<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo nibh, mattis sit amet consequat a, varius vitae metus. Proin pharetra sodales pellentesque.</p> -->
 	</li>
 	<li>
 		<div class="details">
 			<h3>3Codex</h3>
-			<h4>UI/UX Developer - Cairo, Egypt</h4>
-			<h5>July 2016 - Present</h5>
+			<h4>UI Developer - Cairo, Egypt</h4>
+			<h5>July 2016 - August 2016</h5>
 		</div>
 		<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo nibh, mattis sit amet consequat a, varius vitae metus. Proin pharetra sodales pellentesque.</p> -->
 	</li>
 	<li>
 		<div class="details">
 			<h3>Buzlin Inc.</h3>
-			<h4>UI/UX Developer - Cairo, Egypt</h4>
-			<h5>July 2016 - July 2016</h5>
+			<h4>UI Developer - Cairo, Egypt</h4>
+			<h5>July 2016 - August 2016</h5>
 		</div>
 		<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo nibh, mattis sit amet consequat a, varius vitae metus. Proin pharetra sodales pellentesque.</p> -->
 	</li>
 	<li>
 		<div class="details">
 			<h3>Al-yasser Group</h3>
-			<h4>UI/UX Developer - Cairo, Egypt</h4>
+			<h4>UI Developer - Cairo, Egypt</h4>
 			<h5>Oct 2015 - July 2016</h5>
 		</div>
 		<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo nibh, mattis sit amet consequat a, varius vitae metus. Proin pharetra sodales pellentesque.</p> -->
